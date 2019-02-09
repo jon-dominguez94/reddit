3adbc30c6fb39ad2137018513911fc19305380b1
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-08 23:45:34 -0800

    Start next controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render "api/user/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
 end
